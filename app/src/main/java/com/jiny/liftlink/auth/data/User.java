c61bb274ba6d09712949a2ad18cefe106fd10741
김진영 <kjy9866@naver.com>
2025-04-12 17:03:49 +0900

    refactor: Lombok Getter, Setter 적용

--- app/src/main/java/com/jiny/liftlink/auth/data/User.java
+++ app/src/main/java/com/jiny/liftlink/auth/data/User.java
@@ -1,11 +1,14 @@
 package com.jiny.liftlink.auth.data;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class User {
 
     private String name;
-
     private String email;
-
     private String phone;
     private String address;
     private String profileImageUrl;  // 프로필 이미지 URL 추가
@@ -22,40 +25,4 @@ public class User {
         this.profileImageUrl = profileImageUrl;
     }
 
-    // Getters and Setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getProfileImageUrl() {
-        return profileImageUrl;
-    }
-
-    public void setProfileImageUrl(String profileImageUrl) {
-        this.profileImageUrl = profileImageUrl;
-    }
 }
